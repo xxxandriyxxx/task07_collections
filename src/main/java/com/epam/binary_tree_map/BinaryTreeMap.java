cf5969f349aaca4b39838d8ff49791cd953a8440
Andriy <andriymattt@gmail.com>
2019-11-27 23:46:27 +0200

    relocate BinaryTreeMap class, add Node inner class to it

---
+++ src/main/java/com/epam/binary_tree_map/BinaryTreeMap.java
@@ -0,0 +1,101 @@
+package com.epam.binary_tree_map;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class BinaryTreeMap<K extends Comparable, V> implements Map<K, V> {
+
+    private int size = 0;
+    private Node<K, V> root;
+
+    private static class Node<K, V> implements Entry<K, V> {
+
+        private Node<K, V> left;
+        private Node<K, V> right;
+        private K key;
+        private V value;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            V oldValue = this.value;
+            this.value = value;
+            return oldValue;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+}
